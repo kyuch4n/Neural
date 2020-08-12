4cb63fdeaa4eb794756601247c50dd07d8194b08
qiuqi <qiuqi@meituan.com>
2020-08-12 15:21:26 +0800

    feat: support symbol

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,14 @@ import "./App.scss";
 const WAIT = 500;
 const timer = new Timer();
 
+enum Symb {
+  ALL = "ALL",
+}
+
 const Pattern = {
   isEmpty: (input: string) => input.trim() === ">" || input.trim() === "",
-  isCreateCommand: (input: string) => input.indexOf(">") > -1,
+  isCreateCommand: (input: string) => input.startsWith(">"),
+  isSymbolCommand: (input: string) => input.startsWith("@"),
 };
 
 const App: FC = () => {
@@ -46,24 +51,42 @@ const App: FC = () => {
       (input: string) => {
         input = input.trim();
 
-        if (Pattern.isCreateCommand(input)) return;
-
-        if (Pattern.isEmpty(input)) {
-          setTaglist([]);
-          setSelectedTag(null);
-          return;
+        switch (true) {
+          case Pattern.isEmpty(input):
+          case Pattern.isCreateCommand(input):
+            return;
+          case Pattern.isSymbolCommand(input):
+            let symbol = input.slice(1).trim().toUpperCase();
+            switch (symbol) {
+              case Symb.ALL:
+                neuralDB.ready(async () => {
+                  try {
+                    let result: any = await neuralDB.query_tag_by_paging(1, 10000); // 分页是这么用的
+                    let list = result.list;
+                    setTaglist(list);
+                    setSelectedTag(list[0] || null);
+                  } catch (e) {
+                    shake();
+                    console.log(e);
+                  }
+                });
+                return;
+              default:
+                return;
+            }
+          default:
+            neuralDB.ready(async () => {
+              try {
+                let result: any = await neuralDB.match_tag_by_name(input);
+                setTaglist(result);
+                setSelectedTag(result[0] || null);
+              } catch (e) {
+                shake();
+                console.log(e);
+              }
+            });
+            return;
         }
-
-        neuralDB.ready(async () => {
-          try {
-            let result: any = await neuralDB.match_tag_by_name(input);
-            setTaglist(result);
-            setSelectedTag(result[0] || null);
-          } catch (e) {
-            shake();
-            console.log(e);
-          }
-        });
       },
       WAIT,
       {
